6c6fb95afdc03539038fb47bd00bca31be0d94d6
gabrielbraga18 <gabrielfbraga18@gmail.com>
2025-08-13 11:17:36 -0400

    fixing bug on login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,27 @@ import { AppRoutes } from './routes/AppRoutes';
 import { CartProvider } from './context/CartContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <CartProvider>
-      <AppRoutes />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <AppRoutes />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
